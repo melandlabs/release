295fc610b06d8254100f3448ebf622c6a7fd67d9
timi <timi@localhost>
2026-03-16 19:18:58 +0800

    fix: update version to v0.3.0

--- Casks/alloomi.rb
+++ Casks/alloomi.rb
@@ -2,7 +2,7 @@ cask "alloomi" do
   version "0.3.0"
   sha256 "8f14bb487e239fa6b013592b14a4d271bfcf32751be9726462f3bb82c9589ce9"
 
-  url "https://github.com/melandlabs/release/releases/download/0.0.0-test/Alloomi_0.3.0_macOS_aarch64.dmg",
+  url "https://github.com/melandlabs/release/releases/download/v0.3.0/Alloomi_0.3.0_macOS_aarch64.dmg",
       verified: "github.com/melandlabs/release"
 
   name "Alloomi"
